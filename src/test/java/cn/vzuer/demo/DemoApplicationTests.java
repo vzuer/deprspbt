ccb22d85cc1c66a39b9e8b794358849a4b5ad8db
vzuer <vzuers@163.com>
2021-04-28 20:57:42 +0800

    init project

---
+++ src/test/java/cn/vzuer/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.vzuer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
